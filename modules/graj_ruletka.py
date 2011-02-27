716cba6b54a4281bc989674ea72bfab783466d66
Tomasz Kontusz <roverorna@gmail.com>
2011-02-27 20:10:12 +0100

    Nowe moduły

---
+++ modules/graj_ruletka.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__module_class_names__ = ["GraczRuletki"]
+
+from mate import MateModule
+
+class GraczRuletki(MateModule):
+    def __init__(self, mate, config):
+        MateModule.__init__(self, mate, config)
+        self.regex = u'(?i)(' + mate.conf['nick'] + ': teraz ty([!]*))|(^\.krec)$'
+        self.ready = False
+
+    def run(self, mate, nick, msg):
+        if not self.ready and '.krec' in mate.match:
+            self.ready = True
+        elif self.ready and not '.krec' in mate.match:
+            mate.say( '.strzal' )
+            self.ready = False
+
---
+++ modules/wydupiaj.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__module_class_names__ = ["Wydupiaj"]
+
+from mate import MateModule, run_per_minute, noop
+import random
+
+words_wydupiaj = [ 'wydupiaj', 'wypierdalaj', 'spierdalaj' ]
+words_jebajsie = [ 'jebaj sie', u'jebaj się' ]
+
+class Wydupiaj(MateModule):
+    def __init__(self, mate, config):
+        MateModule.__init__(self, mate, config)
+        self.regex = u'(?i)((' + '|'.join(words_wydupiaj + words_jebajsie) + ') *)(' + mate.conf['nick'] + ')([!?]*)$'
+
+    def run(self, mate, nick, msg):
+        if mate.match[1] in words_wydupiaj:
+            mate.reply( random.choice( words_jebajsie ) )
+        else:
+            mate.reply( random.choice( words_wydupiaj) )
+
